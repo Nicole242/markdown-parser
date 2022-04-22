fb5a94b781997908826e29ddcda15e2a335fc5ad
HantianLin <1018753839wl@gmail.com>
2022-04-22 15:46:42 -0700

    Lab 4

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,9 +1,30 @@
 import static org.junit.Assert.*;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.*;
 
 public class MarkdownParseTest {
+    //private MarkdownParse links = new MarkdownParse();
+
     @Test
     public void addition() {
         assertEquals(2, 1 + 1);
+    }
+
+    @Test
+    public void testGetLinks() throws IOException{
+
+        String[] args = new String[] {"/Users/hantian_lin/Documents/GitHub/markdown-parser/test-file.md"};
+        Path fileName = Path.of(args[0]);
+        String content = Files.readString(fileName);
+        
+        //Path test = Path.of("test-file.md");
+        assertEquals(List.of("https://something.com", "some-thing.html"), 
+                            MarkdownParse.getLinks(content));
     }
 }
